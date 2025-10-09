25b31ddf3eddf009e41f7c115b0533a9aef17839
Ssyhaj <118054400+Ssyhaj@users.noreply.github.com>
2025-10-09 18:19:26 +0200

    Update TaxCalculator.py

--- TaxCalculator.py
+++ TaxCalculator.py
@@ -1,111 +1,106 @@
 class TaxCalculator(object):
-    income = 0
-    contractType = ""
-    # social security taxes
-    t_socialSecurity = 0 # 9,76% of the income
-    t_socialSecurityHealth = 0 # 1,5% of the income
-    t_socialSecuritySickness = 0 # 2,45% of the income
- 
-    t_deductibleExpenses = 111.25
-    t_health1 = 0 # 9% of the income
-    t_health2 = 0 # 7,75% of the income
-    t_advance = 0 # advance tax = 18%
-    reducedTax = 46.33 # tax free income 46,33 PLN
-    advanceTax = 0
-    advanceTax0 = 0
+    SOCIAL_SECURITY_TAX = 9.76
+    HEALTH_SOCIAL_SECURITY_TAX = 1.5
+    SICKNESS_SOCIAL_SECURITY_TAX = 2.45
+    HEALTH_TAX_1 = 9.0
+    HEALTH_TAX_2 = 7.75
+    ADVANCE_TAX = 18.0
+    TAX_FREE_INCOME = 46.33 #podaje dane jako stale i uzywam ich jako zmiennych, raczej niz za kazdym razem wpisywac wartosc do obliczen
+    DEDUCTIBLE_EXPENSES = 111.25 #zmieniam nazwy zmiennych na zrozumiale
 
-    @staticmethod
-    def main():
+    def __init__(self): #zmiany z wartosci klasy na wartosci instancji, zeby latwiej bylo to potem refaktorowac
+        self.income = 0.0
+        self.contract_type = ""
+        self.advance_tax = 0.0
+        self.advance_tax_rounded = 0.0
+
+    def main(self):
         try:
-            TaxCalculator.income=float(input("Enter income: "))
-            TaxCalculator.contractType=input("Contract Type: (E)mployment, (C)ivil")[0]
+            self.income=float(input("Enter income: "))
+            self.contractType=input("Contract Type: (E)mployment, (C)ivil")[0]
         except ValueError:
             print("Incorrect")
             return
-        if TaxCalculator.contractType=="E":
+        if self.contractType=="E":
             print("EMPLOYMENT")
-            print("Income ", TaxCalculator.income)
-            d_income = TaxCalculator.calculateIncome(TaxCalculator.income)
-            print("Social security tax: "+"{0:.2f}".format(TaxCalculator.t_socialSecurity))
-            print("Health social security tax: "+"{0:.2f}".format(TaxCalculator.t_socialSecurityHealth))
-            print("Sickness social security tax: "+"{0:.2f}".format(TaxCalculator.t_socialSecuritySickness))
+            print("Income ", self.income)
+            d_income = self.calculateIncome(self.income)
+            print("Social security tax: "+"{0:.2f}".format(self.SOCIAL_SECURITY_TAX))
+            print("Health social security tax: "+"{0:.2f}".format(self.HEALTH_SOCIAL_SECURITY_TAX))
+            print("Sickness social security tax: "+"{0:.2f}".format(self.SICKNESS_SOCIAL_SECURITY_TAX))
             print("Income basis for healt social security: ",d_income)
-            TaxCalculator.calculateOtherTaxes(d_income)
+            self.calculateOtherTaxes(d_income)
             print("Healt social security tax: 9% = " \
-                  +"{0:.2f}".format(TaxCalculator.t_health1)+" 7,75% = "+"{0:.2f}".format(TaxCalculator.t_health2))
-            print("Tax deductible expenses: ",TaxCalculator.t_deductibleExpenses)
-            taxedIncome = d_income - TaxCalculator.t_deductibleExpenses
+                  +"{0:.2f}".format(self.HEALTH_TAX_1)+" 7,75% = "+"{0:.2f}".format(self.HEALTH_TAX_2))
+            print("Tax deductible expenses: ",self.DEDUCTIBLE_EXPENSES)
+            taxedIncome = d_income - self.DEDUCTIBLE_EXPENSES
             taxedIncome0 = float("{0:.0f}".format(taxedIncome))
             print("Income: ",taxedIncome," rounded: "+"{0:.0f}".format(taxedIncome0))
-            TaxCalculator.calculateTax(taxedIncome0)
-            print("Advance tax 18% = ",TaxCalculator.t_advance)
-            print("Tax free income =",TaxCalculator.reducedTax)
-            taxPaid = TaxCalculator.t_advance - TaxCalculator.reducedTax
+            self.calculateTax(taxedIncome0)
+            print("Advance tax 18% = ",self.ADVANCE_TAX)
+            print("Tax free income =",self.TAX_FREE_INCOME)
+            taxPaid = self.ADVANCE_TAX - self.TAX_FREE_INCOME
             print("Reduced tax = "+"{0:.2f}".format(taxPaid))
-            TaxCalculator.calculateAdvanceTax()
-            TaxCalculator.advanceTax0 = float("{0:.0f}".format(TaxCalculator.advanceTax))
-            print("Advance paid tax = "+"{0:.2f}".format(TaxCalculator.advanceTax)+\
-                  " rounded "+"{0:.0f}".format(TaxCalculator.advanceTax0))
-            netIncome = TaxCalculator.income - ((TaxCalculator.t_socialSecurity + TaxCalculator.t_socialSecurityHealth \
-                            + TaxCalculator.t_socialSecuritySickness) + TaxCalculator.t_health1 + TaxCalculator.advanceTax0)
+            self.calculateAdvanceTax()
+            self.advanceTax0 = float("{0:.0f}".format(self.advanceTax))
+            print("Advance paid tax = "+"{0:.2f}".format(self.advanceTax)+\
+                  " rounded "+"{0:.0f}".format(self.advanceTax0))
+            netIncome = self.income - ((self.SOCIAL_SECURITY_TAX + self.HEALTH_SOCIAL_SECURITY_TAX \
+                            + self.SICKNESS_SOCIAL_SECURITY_TAX) + self.HEALTH_TAX_1 + self.advanceTax0)
             print()
             print("Net income = "+"{0:.2f}".format(netIncome))
             
 
-        elif TaxCalculator.contractType=="C":
+        elif self.contractType=="C":
             print("CIVIL")
-            print("Income",TaxCalculator.income)
-            d_income = TaxCalculator.calculateIncome(TaxCalculator.income)
-            print("Social security tax: "+"{0:.2f}".format(TaxCalculator.t_socialSecurity))
-            print("Health social security tax: "+"{0:.2f}".format(TaxCalculator.t_socialSecurityHealth))
-            print("Sickness social security tax  "+"{0:.2f}".format(TaxCalculator.t_socialSecuritySickness))
+            print("Income",self.income)
+            d_income = self.calculateIncome(self.income)
+            print("Social security tax: "+"{0:.2f}".format(self.SOCIAL_SECURITY_TAX))
+            print("Health social security tax: "+"{0:.2f}".format(self.HEALTH_SOCIAL_SECURITY_TAX))
+            print("Sickness social security tax  "+"{0:.2f}".format(self.SICKNESS_SOCIAL_SECURITY_TAX))
             print("Income for calculating health security tax: ",d_income)
-            TaxCalculator.calculateOtherTaxes(d_income)
+            self.calculateOtherTaxes(d_income)
             print("Health security tax: 9% = " \
-                  +"{0:.2f}".format(TaxCalculator.t_health1)+" 7,75% = "+"{0:.2f}".format(TaxCalculator.t_health2))
-            TaxCalculator.reducedTax = 0
-            TaxCalculator.t_deductibleExpenses = (d_income * 20) / 100
-            print("Tax deductible expenses = ",TaxCalculator.t_deductibleExpenses)
-            taxedIncome = d_income - TaxCalculator.t_deductibleExpenses
+                  +"{0:.2f}".format(self.HEALTH_TAX_1)+" 7,75% = "+"{0:.2f}".format(self.HEALTH_TAX_2))
+            self.TAX_FREE_INCOME = 0
+            self.DEDUCTIBLE_EXPENSES = (d_income * 20) / 100
+            print("Tax deductible expenses = ",self.DEDUCTIBLE_EXPENSES)
+            taxedIncome = d_income - self.DEDUCTIBLE_EXPENSES
             taxedIncome0 = float("{0:.0f}".format(taxedIncome))
             print("income to be taxed: ",taxedIncome," rounded: "+"{0:.0f}".format(taxedIncome0))
-            TaxCalculator.calculateTax(taxedIncome0)
-            print("Advance tax 18% =",TaxCalculator.t_advance)
-            taxPaid = TaxCalculator.t_advance            
+            self.calculateTax(taxedIncome0)
+            print("Advance tax 18% =",self.ADVANCE_TAX)
+            taxPaid = self.ADVANCE_TAX            
             print("Already paid tax = "+"{0:.2f}".format(taxPaid))
-            TaxCalculator.calculateAdvanceTax()
-            TaxCalculator.advanceTax0 = float("{0:.0f}".format(TaxCalculator.advanceTax))
-            print("Advance tax = "+"{0:.2f}".format(TaxCalculator.advanceTax)+\
-                  " rounded "+"{0:.0f}".format(TaxCalculator.advanceTax0))
-            netIncome = TaxCalculator.income - ((TaxCalculator.t_socialSecurity + TaxCalculator.t_socialSecurityHealth \
-                          + TaxCalculator.t_socialSecuritySickness) + TaxCalculator.t_health1 + TaxCalculator.advanceTax0)
+            self.calculateAdvanceTax()
+            self.advanceTax0 = float("{0:.0f}".format(self.advanceTax))
+            print("Advance tax = "+"{0:.2f}".format(self.advanceTax)+\
+                  " rounded "+"{0:.0f}".format(self.advanceTax0))
+            netIncome = self.income - ((self.SOCIAL_SECURITY_TAX + self.HEALTH_SOCIAL_SECURITY_TAX \
+                          + self.SICKNESS_SOCIAL_SECURITY_TAX) + self.HEALTH_TAX_1 + self.advanceTax0)
             print()
             print("Net income = "+"{0:.2f}".format(netIncome))
              
         else:
             print("Unknowne type of contract!")
-            
-    @staticmethod
-    def calculateAdvanceTax():
-        TaxCalculator.advanceTax=TaxCalculator.t_advance - TaxCalculator.t_health2 - TaxCalculator.reducedTax
 
-    @staticmethod
-    def calculateTax(income):
-        TaxCalculator.t_advance = (income * 18) / 100
+    def calculateAdvanceTax(self):
+        self.advanceTax=self.ADVANCE_TAX - self.HEALTH_TAX_2 - self.TAX_FREE_INCOME
 
-    @staticmethod
-    def calculateIncome(income):
-        TaxCalculator.t_socialSecurity = (income * 9.76) / 100        
-        TaxCalculator.t_socialSecurityHealth = (income * 1.5) / 100
-        TaxCalculator.t_socialSecuritySickness = (income * 2.45) / 100
-        return (income - TaxCalculator.t_socialSecurity- TaxCalculator.t_socialSecurityHealth - TaxCalculator.t_socialSecuritySickness)
+    def calculateTax(self, income):
+        self.ADVANCE_TAX = (income * 18) / 100
 
-    @staticmethod
-    def calculateOtherTaxes(income):
-        TaxCalculator.t_health1 = (income * 9) / 100
-        TaxCalculator.t_health2 = (income * 7.75) / 100
+    def calculateIncome(self, income):
+        self.SOCIAL_SECURITY_TAX = (income * 9.76) / 100        
+        self.HEALTH_SOCIAL_SECURITY_TAX = (income * 1.5) / 100
+        self.SICKNESS_SOCIAL_SECURITY_TAX = (income * 2.45) / 100
+        return (income - self.SOCIAL_SECURITY_TAX- self.HEALTH_SOCIAL_SECURITY_TAX - self.SICKNESS_SOCIAL_SECURITY_TAX)
+
+    def calculateOtherTaxes(self, income):
+        self.HEALTH_TAX_1 = (income * 9) / 100
+        self.HEALTH_TAX_2 = (income * 7.75) / 100
 
         
 if __name__=='__main__':
-    TaxCalculator.main()
-
+    calc = TaxCalculator()
+    calc.main()
