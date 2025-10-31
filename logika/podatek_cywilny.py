c130794670ecde4f5da1a6b1eab205301d9773f7
Szymon Haj <118054400+Ssyhaj@users.noreply.github.com>
2025-10-31 21:05:39 +0100

    Refactor tax calculation methods for clarity

--- logika/podatek_cywilny.py
+++ logika/podatek_cywilny.py
@@ -8,15 +8,16 @@ class PodatekCywilny(ObliczaniePodatku):
     PROCENT_WTORNEGO_PODATKU_ZDROWOTNEGO = 7.75
     PROCENT_PODATKU_ZALICZKOWEGO = 18
     PROCENT_PODATKU_KOSZTOW_UZYSKANIA = 20
+    PROCENT = 100
     
     def __init__(self, dochod):
         super().__init__(dochod)
         self.dochod_wolny_od_podatku = 0
     
     def oblicz_podatki_ubezpieczenia_spolecznego(self):
-        self.podatek_ubezpieczenia_spolecznego = (self.dochod * self.PROCENT_PODATKU_UBEZPIECZENIA_SPOLECZNEGO) / 100
-        self.podatek_ubezpieczenia_zdrowotnego_spolecznego = (self.dochod * self.PROCENT_PODATKU_UBEZPIECZENIA_ZDROWOTNEGO_SPOLECZNEGO) / 100
-        self.podatek_ubezpieczenia_chorobowego = (self.dochod * self.PROCENT_PODATKU_UBEZPIECZENIA_CHOROBOWEGO) / 100
+        self.podatek_ubezpieczenia_spolecznego = (self.dochod * self.PROCENT_PODATKU_UBEZPIECZENIA_SPOLECZNEGO) / self.PROCENT
+        self.podatek_ubezpieczenia_zdrowotnego_spolecznego = (self.dochod * self.PROCENT_PODATKU_UBEZPIECZENIA_ZDROWOTNEGO_SPOLECZNEGO) / self.PROCENT
+        self.podatek_ubezpieczenia_chorobowego = (self.dochod * self.PROCENT_PODATKU_UBEZPIECZENIA_CHOROBOWEGO) / self.PROCENT
         self._zapisz_podatek_ubezpieczenia_spolecznego()
         self.dochod = self.dochod - self.podatek_ubezpieczenia_spolecznego - self.podatek_ubezpieczenia_zdrowotnego_spolecznego - self.podatek_ubezpieczenia_chorobowego
         self._zapisz_podstawowy_dochod()
@@ -67,4 +68,5 @@ class PodatekCywilny(ObliczaniePodatku):
         self._zapisz_dochod_netto()
     
     def _zapisz_dochod_netto(self):
+
         self.dane_podatkowe["Net income"] = self.dochod_netto
